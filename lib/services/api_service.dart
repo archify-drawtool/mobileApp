38479fccf8ca4ed496095d78f6c35663ef6a4ef5
ant-george <agross@live.nl>
2026-03-23 13:01:01 +0100

    Update AndroidManifest and ApiService for internet access and local API URL

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -4,7 +4,7 @@ import 'dart:convert';
 class ApiService {
   static const String baseUrl = String.fromEnvironment(
     'API_URL',
-    defaultValue: 'http://localhost:8000/api',
+    defaultValue: 'http://10.0.2.2:8080/api',
   );
 
   Future<String> checkHealth() async {
